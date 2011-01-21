e5be8888a0bf401017121033037ba79cd7a8b47e
unknown <SATYA@.(none)>
2011-01-21 15:00:34 +0530

    changed the names

--- SumOfAllMultipleof3n5/src/SumOfAllMultiple3N5Test.java
+++ SumOfAllMultipleof3n5/src/SumOfAllMultiple3N5Test.java
@@ -1,27 +1,10 @@
 import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
-/**
- * Created by IntelliJ IDEA.
- * User: jeetendra
- * Date: 19 Jan, 2011
- * Time: 6:37:44 PM
- * To change this template use File | Settings | File Templates.
- */
-
 public class SumOfAllMultiple3N5Test {
 
     @Test
-    public void testSumOfMultiple35(){
-
-        SumOfAllMultiple3N5 sumOfAllMultiple3N5;
-        sumOfAllMultiple3N5 = new SumOfAllMultiple3N5();
-
-        int sum = sumOfAllMultiple3N5.sumOfMultiple35();
-
-        System.out.println("sum :" +sum);
-
-        assertEquals(233168, sum);
-
+    public void testSumOfMultiplesOf3And5(){
+        assertEquals(233168, new NumberRange(1,1000).sumOfMultiplesOf(3, 5));
     }
 }
